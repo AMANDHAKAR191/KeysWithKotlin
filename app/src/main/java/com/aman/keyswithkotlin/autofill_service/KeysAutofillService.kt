e74fb911c1593c5a8ec311204276bd301f5d4bb7
kiran <kirandhaker123@gmail.com>
2023-08-27 22:04:08 +0530

    saveUi info is working now made some improvement

--- app/src/main/java/com/aman/keyswithkotlin/autofill_service/KeysAutofillService.kt
+++ app/src/main/java/com/aman/keyswithkotlin/autofill_service/KeysAutofillService.kt
@@ -85,73 +85,16 @@ class KeysAutofillService : AutofillService() {
 
         println("AUTOFILL_HINT_USERNAME: ${View.AUTOFILL_HINT_USERNAME}")
         println("AUTOFILL_HINT_PASSWORD: ${View.AUTOFILL_HINT_PASSWORD}")
-        //traverse the structure
-//        for (i in 0 until structure.windowNodeCount) {
-//            var foundUsernameLabel = false
-//            var foundPasswordLabel = false
-//            val windowNode = structure.getWindowNodeAt(i)
-//            val viewNode = windowNode.rootViewNode
-//            traverseViewStructure(viewNode) { node ->
-//                node.idEntry?.let { nodeIdEntry ->
-//                    //this is used previous view of textField is contains the hints or not
-//                    if (nodeIdEntry.contains("user") || nodeIdEntry.contains("email")) {
-//                        foundUsernameLabel = true
-//                        println("nodeIdEntry: username")
-//                    }
-//                    if (nodeIdEntry.contains("pass")) {
-//                        foundPasswordLabel = true
-//                        println("nodeIdEntry: password")
-//                    }
-//                }
-//                node.htmlInfo?.attributes?.forEach { pair ->
-//                    when (pair.first) {
-//                        "label" -> {
-//                            if (pair.second.contains("Username", ignoreCase = true)) {
-//                                userNameId = node.autofillId
-//                                // This is a username field
-//                            } else if (pair.second.contains("Password", ignoreCase = true)) {
-//                                passwordId = node.autofillId
-//                                // This is a password field
-//                            }
-//                        }
-//                        // ... You can add more cases
-//                    }
-//                }
-//
-//                println("11: ${Arrays.toString(node.autofillHints)}")
-//                println("1: ${node.autofillId}, 2: ${node.autofillHints} 3: ${node.idEntry} 4: ${node.htmlInfo?.tag} 5: ${node.htmlInfo?.attributes}")
-//
-//                //this check if autofill is then go with 1st block otherwise go with second block
-//                if (node.autofillHints?.contains(View.AUTOFILL_HINT_USERNAME) == true) {
-//                    userNameId = node.autofillId
-//                    println("userNameId: $userNameId")
-//                } else if (foundUsernameLabel && node.inputType == InputType.TYPE_CLASS_TEXT) {
-//                    userNameId = node.autofillId
-//                    foundUsernameLabel = false
-//                }
-//                if (node.autofillHints?.any {
-//                        it.contains(
-//                            View.AUTOFILL_HINT_PASSWORD,
-//                            ignoreCase = true
-//                        )
-//                    } == true) {
-//                    passwordId = node.autofillId
-//                    println("passwordId: $passwordId")
-//                } else if (foundPasswordLabel && node.inputType == InputType.TYPE_CLASS_TEXT) {
-//                    passwordId = node.autofillId
-//                    foundPasswordLabel = false
-//                }
-//            }
-//        }
 
+        // Traverse the structure and find the AutofillIds
         traverseStructure(
             structure,
             requestType = RequestType.OnFillRequest,
-            setUsernameId = {_userNameId->
+            setUsernameId = { _userNameId ->
                 userNameId = _userNameId
             },
-            setPasswordId = {_passwordId->
-            passwordId = _passwordId
+            setPasswordId = { _passwordId ->
+                passwordId = _passwordId
             }
         )
 
@@ -219,11 +162,9 @@ class KeysAutofillService : AutofillService() {
                     //this is used previous view of textField is contains the hints or not
                     if (nodeIdEntry.contains("user") || nodeIdEntry.contains("email")) {
                         foundUsernameLabel = true
-                        println("nodeIdEntry: username")
                     }
                     if (nodeIdEntry.contains("pass")) {
                         foundPasswordLabel = true
-                        println("nodeIdEntry: password")
                     }
                 }
                 node.htmlInfo?.attributes?.forEach { pair ->
@@ -257,8 +198,8 @@ class KeysAutofillService : AutofillService() {
                     }
                 }
 
-                println("11: ${Arrays.toString(node.autofillHints)}")
-                println("1: ${node.autofillId}, 2: ${node.autofillHints} 3: ${node.idEntry} 4: ${node.htmlInfo?.tag} 5: ${node.htmlInfo?.attributes}")
+//                println("11: ${Arrays.toString(node.autofillHints)}")
+//                println("1: ${node.autofillId}, 2: ${node.autofillHints} 3: ${node.idEntry} 4: ${node.htmlInfo?.tag} 5: ${node.htmlInfo?.attributes}")
 
                 //this check if autofill is then go with 1st block otherwise go with second block
                 if (node.autofillHints?.contains(View.AUTOFILL_HINT_USERNAME) == true) {
@@ -271,7 +212,6 @@ class KeysAutofillService : AutofillService() {
                             setUsernameValue(value.textValue.toString())
                         }
                     }
-//                    println("userNameId: $userName")
                 } else if (foundUsernameLabel && node.inputType == InputType.TYPE_CLASS_TEXT) {
                     if (setUsernameId != null && requestType == RequestType.OnFillRequest) {
                         setUsernameId(node.autofillId!!)
@@ -285,7 +225,12 @@ class KeysAutofillService : AutofillService() {
                     foundUsernameLabel = false
                 }
 
-                if (node.autofillHints?.any { it.contains(View.AUTOFILL_HINT_PASSWORD, ignoreCase = true) } == true) {
+                if (node.autofillHints?.any {
+                        it.contains(
+                            View.AUTOFILL_HINT_PASSWORD,
+                            ignoreCase = true
+                        )
+                    } == true) {
                     if (setPasswordId != null && requestType == RequestType.OnFillRequest) {
                         setPasswordId(node.autofillId!!)
                     }
@@ -295,7 +240,6 @@ class KeysAutofillService : AutofillService() {
                             setPasswordValue(value.textValue.toString())
                         }
                     }
-//                    println("passwordId: $password")
                 } else if (foundPasswordLabel && node.inputType == InputType.TYPE_CLASS_TEXT) {
                     if (setPasswordId != null && requestType == RequestType.OnFillRequest) {
                         setPasswordId(node.autofillId!!)
@@ -323,99 +267,6 @@ class KeysAutofillService : AutofillService() {
         var password: String? = null
 
         // Traverse the structure and find the AutofillValues
-//        for (i in 0 until structure.windowNodeCount) {
-//            val windowNode = structure.getWindowNodeAt(i)
-//            val viewNode = windowNode.rootViewNode
-////            traverseViewStructure(viewNode) { node ->
-////                if (node.autofillHints?.contains(View.AUTOFILL_HINT_USERNAME) == true) {
-////                    val value = node.autofillValue
-////                    if (value?.isText!! && value?.textValue != null) {
-////                        userName = value.textValue.toString()
-////                    }
-////                    println("userName: $userName")
-////                }
-////                if (node.autofillHints?.contains(View.AUTOFILL_HINT_PASSWORD) == true) {
-////                    val value = node.autofillValue
-////                    if (value?.isText!! && value?.textValue != null) {
-////                        password = value.textValue.toString()
-////                    }
-////                    println("password: $password")
-////                }
-////            }
-//
-//            var foundUsernameLabel = false
-//            var foundPasswordLabel = false
-//            traverseViewStructure(viewNode) { node ->
-//                node.idEntry?.let { nodeIdEntry ->
-//                    //this is used previous view of textField is contains the hints or not
-//                    if (nodeIdEntry.contains("user") || nodeIdEntry.contains("email")) {
-//                        foundUsernameLabel = true
-//                        println("nodeIdEntry: username")
-//                    }
-//                    if (nodeIdEntry.contains("pass")) {
-//                        foundPasswordLabel = true
-//                        println("nodeIdEntry: password")
-//                    }
-//                }
-//                node.htmlInfo?.attributes?.forEach { pair ->
-//                    when (pair.first) {
-//                        "label" -> {
-//                            if (pair.second.contains("Username", ignoreCase = true)) {
-//                                val value = node.autofillValue
-//                                if (value?.isText!! && value?.textValue != null) {
-//                                    userName = value.textValue.toString()
-//                                }
-//                                // This is a username field
-//                            } else if (pair.second.contains("Password", ignoreCase = true)) {
-//                                val value = node.autofillValue
-//                                if (value?.isText!! && value?.textValue != null) {
-//                                    password = value.textValue.toString()
-//                                }
-//                                // This is a password field
-//                            }
-//                        }
-//                        // ... You can add more cases
-//                    }
-//                }
-//
-//                println("11: ${Arrays.toString(node.autofillHints)}")
-//                println("1: ${node.autofillId}, 2: ${node.autofillHints} 3: ${node.idEntry} 4: ${node.htmlInfo?.tag} 5: ${node.htmlInfo?.attributes}")
-//
-//                //this check if autofill is then go with 1st block otherwise go with second block
-//                if (node.autofillHints?.contains(View.AUTOFILL_HINT_USERNAME) == true) {
-//                    val value = node.autofillValue
-//                    if (value?.isText!! && value?.textValue != null) {
-//                        userName = value.textValue.toString()
-//                    }
-//                    println("userNameId: $userName")
-//                } else if (foundUsernameLabel && node.inputType == InputType.TYPE_CLASS_TEXT) {
-//                    val value = node.autofillValue
-//                    if (value?.isText!! && value?.textValue != null) {
-//                        userName = value.textValue.toString()
-//                    }
-//                    foundUsernameLabel = false
-//                }
-//                if (node.autofillHints?.any {
-//                        it.contains(
-//                            View.AUTOFILL_HINT_PASSWORD,
-//                            ignoreCase = true
-//                        )
-//                    } == true) {
-//                    val value = node.autofillValue
-//                    if (value?.isText!! && value?.textValue != null) {
-//                        password = value.textValue.toString()
-//                    }
-//                    println("passwordId: $password")
-//                } else if (foundPasswordLabel && node.inputType == InputType.TYPE_CLASS_TEXT) {
-//                    val value = node.autofillValue
-//                    if (value?.isText!! && value?.textValue != null) {
-//                        password = value.textValue.toString()
-//                    }
-//                    foundPasswordLabel = false
-//                }
-//            }
-//        }
-
         traverseStructure(
             structure,
             requestType = RequestType.OnSaveRequest,
@@ -434,15 +285,11 @@ class KeysAutofillService : AutofillService() {
             applicationContext.startActivity(intent1)
         }
 
-        // TODO: Here you would save the username and password.
-        // Maybe call an API or store it in a secure place like Android Keystore.
-
-        // Notify the system that the save operation was a success
         callback.onSuccess()
     }
 
 }
 
-enum class RequestType{
+enum class RequestType {
     OnFillRequest, OnSaveRequest
 }
