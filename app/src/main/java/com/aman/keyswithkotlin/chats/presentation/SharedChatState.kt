c126a9f7ab79da05b35fa3f2195f9666d6c2231d
kiran <kirandhaker123@gmail.com>
2023-08-27 23:12:29 +0530

    app icon changed

--- app/src/main/java/com/aman/keyswithkotlin/chats/presentation/SharedChatState.kt
+++ app/src/main/java/com/aman/keyswithkotlin/chats/presentation/SharedChatState.kt
@@ -1,6 +1,5 @@
 package com.aman.keyswithkotlin.chats.presentation
 
-import com.aman.keyswithkotlin.chats.domain.model.Person
 import com.aman.keyswithkotlin.chats.domain.model.UserPersonalChatList
 
 data class SharedChatState(
