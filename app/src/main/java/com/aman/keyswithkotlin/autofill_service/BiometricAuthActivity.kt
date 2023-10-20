6b6073a41d37a99003e13637b02f5565167961a1
kiran <kirandhaker123@gmail.com>
2023-10-21 02:33:42 +0530

    logo update

--- app/src/main/java/com/aman/keyswithkotlin/autofill_service/BiometricAuthActivity.kt
+++ app/src/main/java/com/aman/keyswithkotlin/autofill_service/BiometricAuthActivity.kt
@@ -23,12 +23,19 @@ import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import androidx.compose.ui.window.SecureFlagPolicy
 import androidx.core.app.ActivityCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.aman.keyswithkotlin.passwords.domain.model.Password
@@ -85,17 +92,23 @@ class BiometricAuthActivity : ComponentActivity() {
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun ShowPasswordList(passwordList1: List<Password>) {
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            LazyColumn {
-                items(passwordList1) {
-                    SearchedPasswordItem(
-                        password = it,
-                        onItemClick = { onYes(listOf(it)) }
-                    )
+        AlertDialog(
+            modifier = Modifier.size(height = 500.dp, width = 300.dp),
+            onDismissRequest = { /*TODO*/ }) {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                LazyColumn {
+                    items(passwordList1) {
+                        SearchedPasswordItem(
+                            password = it,
+                            onItemClick = { onYes(listOf(it)) }
+                        )
+                    }
                 }
             }
+
         }
     }
 
